65a76c9e749293f7d6da3381290f52a01c033d07
anschek <anschek@yandex.ru>
2024-09-05 19:40:49 +0300

    layout: added sample template of class Task

---
+++ MultithreadedSimulator/Task.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+
+//TODO
+//add logging
+//exception handling
+
+template <typename T, typename ProccessedData>
+class Task {
+public:
+	Task(T(*funPtr)()) {
+
+	}	
+	Task(T(*funPtr)(ProccessedData), ProccessedData& data) {
+
+	}
+	//process() - data processing
+	// return<T> fun(data)
+	T Process() {
+
+	}
+
+	//get/set on private fields (parameters)
+private:
+	ProccessedData& data_;
+	T (*funcPtr_)(ProccessedData);
+	T (*funcPtrVoid_)();
+
+	//parameters: 
+	//task priority, 
+	//max execution time, 
+	//id, 
+	//task state/status (in progress, completed, error, etc.)
+};
