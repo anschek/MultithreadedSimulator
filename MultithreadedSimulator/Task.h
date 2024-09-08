f9a77756e84e0a97b6ad9c049a529a8380ea11e1
anschek <anschek@yandex.ru>
2024-09-08 14:59:33 +0300

    feat: added overloads to method Process for get result with value/void arg, added factory fun to creation object Task with void arg

--- MultithreadedSimulator/Task.h
+++ MultithreadedSimulator/Task.h
@@ -3,39 +3,45 @@
 #include <future>
 #include <utility>
 #include <type_traits>
+#include <optional>
 
 //TODO
 //add logging
 //exception handling
+//make it possible to get result when calling Process with async execute (now it returns not result T, but obj future<T>)
 
 template <typename Func, typename Arg>
 class Task {
 public:
-	using ReturnType = std::invoke_result_t<Func, Arg>;
+	using ReturnType = std::invoke_result_t<Func, std::decay_t<Arg>>;
 
 	Task(Func funPtr, Arg&& data)
-		:funPtr_(funPtr), funPtrVoid_(nullptr), data_(std::forward<Arg>(data)) {}
+		:funPtr_(funPtr), data_(std::forward<Arg>(data)) {}
 	Task(Func funPtr)
-		:funPtrVoid_(funPtr), funPtr_(nullptr) {}
+		:funPtr_(nullptr), data_(std::nullopt) {}
 
-	std::future<ReturnType> Process() {
-		asyncFutureResult_ = std::async(std::launch::async, funPtr_, std::ref(data_));
-			//(funPtrVoid_ == nullptr)
-			//? std::async(std::launch::async, funPtr_, std::ref(data_))
-			//: std::async(std::launch::async, funPtrVoid_ );
-		return std::move(asyncFutureResult_);
+	template <typename T = Arg>
+	std::enable_if_t<!std::is_same_v<T, void>, std::future<ReturnType>>
+		Process() {
+		return std::async(std::launch::async, funPtr_, std::ref(*data_));
 	}
-
+	template <typename T = Arg>
+	std::enable_if_t<std::is_same_v<T, void>, std::future<ReturnType>>
+		Process() {
+		return std::async(std::launch::async, funPtr_);
+	}
 	//get/set on private fields (parameters)
-	~Task()
-	{
-		if (asyncFutureResult_.valid()) asyncFutureResult_.get();
-	}
+
+	//~Task()
+	//{
+	//	if (asyncFutureResult_.valid()) {
+	//		asyncFutureResult_.get();
+	//	} 
+	//}
 private:
-	std::decay_t<Arg> data_;
+	std::optional<std::decay_t<Arg>> data_;
 	Func funPtr_;
-	Func funPtrVoid_;
-	std::future<ReturnType> asyncFutureResult_;
+	//std::future<ReturnType> asyncFutureResult_;
 	
 	//parameters: 
 	//task priority, 
@@ -49,33 +55,7 @@ Task<Func, Arg> MakeTask(Func funcPtr, Arg&& data) {
 	return Task<Func, Arg>(funcPtr, std::forward<Arg>(data));
 }
 
-
-
-template <typename Func, typename Arg>
-class Task1 {
-public:
-    using ReturnType = std::invoke_result_t<Func, Arg>;
-
-    Task1(Func funPtr, Arg&& data)
-        : funcPtr_(funPtr), data_(std::forward<Arg>(data)) {}
-
-    ReturnType Process() {
-        std::future<ReturnType> asyncFutureResult = std::async(std::launch::async, funcPtr_, std::ref(data_));
-        return asyncFutureResult.get();//!!!
-    }
-
-    ~Task1() {
-        if (curThread_.joinable()) curThread_.join();
-    }
-
-private:
-    Func funcPtr_;
-    std::decay_t<Arg> data_;
-    std::thread curThread_;
-};
-
-// Фабричная функция для автоматического определения типов
-template <typename Func, typename Arg>
-auto MakeTask1(Func func, Arg&& data) {
-    return Task1<Func, Arg>(func, std::forward<Arg>(data));
+template <typename Func>
+Task<Func, void> MakeTask(Func funcPtr) {
+	return Task<Func, void>(funcPtr);
 }
