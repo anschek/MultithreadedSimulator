1f7e74d5e87cb7517ee66746597ac0a8ee5a07b5
anschek <anschek@yandex.ru>
2024-09-08 16:10:51 +0300

    feat: added new task parametres

--- MultithreadedSimulator/Task.h
+++ MultithreadedSimulator/Task.h
@@ -1,54 +1,99 @@
 ﻿#pragma once
-#include <thread>
 #include <future>
-#include <utility>
-#include <type_traits>
 #include <optional>
+#include <thread>
+#include <type_traits>
+#include <utility>
 
 //TODO
-//add logging
-//exception handling
-//make it possible to get result when calling Process with async execute (now it returns not result T, but obj future<T>)
+//add getters to priority, state, id
+//modify state in process
+//add try/catch on process/get result/destructor
+//split to files
+//unit tests
+
+
+enum TaskPriority {
+	HIGH,
+	NORMAL,
+	LOW
+};
 
 template <typename Func, typename Arg>
 class Task {
 public:
 	using ReturnType = std::invoke_result_t<Func, std::decay_t<Arg>>;
 
+	Task(Func funPtr, Arg&& data, TaskPriority task_priority)
+		:funPtr_(funPtr), data_(std::forward<Arg>(data)), task_priority_(task_priority),
+		task_id_(++current_tasks_count), task_state_(UNSTARTED) {}
+	
+	Task(Func funPtr, TaskPriority task_priority)
+		:funPtr_(funPtr), data_(std::nullopt), task_priority_(task_priority),
+			task_id_(++current_tasks_count), task_state_(UNSTARTED) {}
+
 	Task(Func funPtr, Arg&& data)
-		:funPtr_(funPtr), data_(std::forward<Arg>(data)) {}
+		: Task(funPtr, std::forward<Arg>(data), NORMAL) {}
+
 	Task(Func funPtr)
-		:funPtr_(nullptr), data_(std::nullopt) {}
+		:funPtr_(funPtr, NORMAL) {}
 
 	template <typename T = Arg>
-	std::enable_if_t<!std::is_same_v<T, void>, std::future<ReturnType>>
+	std::enable_if_t<!std::is_same_v<T, void>, void>
 		Process() {
-		return std::async(std::launch::async, funPtr_, std::ref(*data_));
+		asyncFutureResult_ = std::async(std::launch::async, funPtr_, std::ref(*data_));
 	}
-	template <typename T = Arg>
-	std::enable_if_t<std::is_same_v<T, void>, std::future<ReturnType>>
-		Process() {
-		return std::async(std::launch::async, funPtr_);
-	}
-	//get/set on private fields (parameters)
 
-	//~Task()
-	//{
-	//	if (asyncFutureResult_.valid()) {
-	//		asyncFutureResult_.get();
-	//	} 
-	//}
+	template <typename T = Arg>
+	std::enable_if_t<std::is_same_v<T, void>, void>
+		Process() {
+		asyncFutureResult_ = std::async(std::launch::async, funPtr_);
+	}
+
+	ReturnType GetResult() {
+		if (asyncFutureResult_.valid()) {
+			return asyncFutureResult_.get();
+		}else {
+			return ReturnType{};
+		}
+	}
+	//get/set on private fields (parameters): id, state, status (read only)
+
+	~Task()
+	{
+		if (asyncFutureResult_.valid()) {
+			asyncFutureResult_.get();
+		} 
+	}
+
+	enum TaskState {
+		UNSTARTED,
+		IN_PROGRESS,
+		COMPLETED,
+		ERROR
+	};
+
 private:
 	std::optional<std::decay_t<Arg>> data_;
 	Func funPtr_;
-	//std::future<ReturnType> asyncFutureResult_;
-	
-	//parameters: 
-	//task priority, 
-	//max execution time, 
-	//id, 
-	//task state/status (in progress, completed, error, etc.)
+	std::future<ReturnType> asyncFutureResult_;
+
+	TaskPriority task_priority_;
+	unsigned task_id_;
+	TaskState task_state_;
+
+	static inline unsigned current_tasks_count = 0;
 };
+
+template <typename Func, typename Arg>
+Task<Func, Arg> MakeTask(Func funcPtr, Arg&& data, TaskPriority task_priority) {
+	return Task<Func, Arg>(funcPtr, std::forward<Arg>(data), task_priority);
+}
+
+template <typename Func>
+Task<Func, void> MakeTask(Func funcPtr, TaskPriority task_priority) {
+	return Task<Func, void>(funcPtr, task_priority);
+}
 
 template <typename Func, typename Arg>
 Task<Func, Arg> MakeTask(Func funcPtr, Arg&& data) {
